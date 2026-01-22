e0c5aa0afc75ed69a8850937603cbd7da53de009
rekha <rekharoyal282@gmail.com>
2026-01-22 13:21:57 +0530

    Created a first js file

---
+++ logical.js
@@ -0,0 +1,16 @@
+a = true;
+b = false;
+console.log(a&&b);
+console.log(a||b);
+console.log(!a);
+
+var arr = [10,20,30,40];
+console.log(arr);
+console.log(typeof arr);
+
+var obj = {
+    name:"rekha",
+    age:20
+}
+console.log(obj);
+console.log(typeof obj);
