b94053d971ff80ae368daeccc24cca692a43dfd2
rekha <rekharoyal282@gmail.com>
2026-01-31 10:45:15 +0530

    frontend

--- script.js
+++ script.js
@@ -23,6 +23,112 @@ console.log(typeof nu);
 console.log(typeof big);
 console.log(typeof symbol);
 
+var obj = {
+    name:"rekha",
+    age:20
+}
+for(let i in obj){
+    console.log(i,obj[i]);
+}
 
 
+let arr = [10,20,30,40];
+var double_arr=arr.map((i)=>(i*2));
+console.log(arr);
 
+var even = arr.filter((i)=>(i%2===0))
+console.log(even);
+
+var total = arr.reduce((sum,i)=>(sum+i),0);
+console.log(total);
+
+var total_sum = arr.map((num)=>num*2).filter((num)=>(num%2===0)).reduce((sum,num)=>(sum+num));
+console.log(total_sum)
+
+var n =7;
+var flag=true;
+if(n<=1) flag = false;
+else{
+    for(let i=2;i<=n/2;i++){
+        if(n%i===0){
+            flag=false;
+            break;
+        }
+    }
+}
+console.log(flag?"prime":"not prime");
+
+var name="rekha";
+console.log(`My name is ${name}`);
+
+arr.forEach((value,index)=>{
+    console.log(index,value);
+})
+
+//call by function
+var add=(a,b,callback)=>{
+    var result=a+b;
+    callback(result);
+}
+add(10,20,(result)=>
+{
+        console.log(result);
+})
+
+var main=(callBack)=>{
+    console.log("I am main");
+    callBack()
+}
+var demo =()=>{
+    console.log("I am callBack");
+}
+var main=(myFunction)=>{
+    myFunction()
+}
+main(demo);
+
+var promise = new Promise((resolve,reject)=>{
+    var success = false;
+    if(success){
+        resolve("Promise Resolved");
+    }
+else{
+    reject("Promise Rejected");
+    }
+})
+promise.then((res)=>console.log(res))
+.catch((err)=>console.log(err))
+
+
+var promise = new Promise((resolve,reject)=>{
+    var success = true;
+    if(success){
+        setTimeout(()=>{
+            resolve("Promise Resolved")
+        },5000)
+    }
+else{
+    reject("Promise Rejected");
+    }
+})
+promise.then((res)=>console.log(res))
+.catch((err)=>console.log(err))
+
+// const getData =()=>{
+//     return fetch('https://jsonplaceholder.typicode.com/posts')
+// }
+// getData().then((res)=>res.json())
+// .then((data)=>console.log(data))
+// .catch(err=>console.log(err))
+
+const getData=async()=>{
+    try{
+   var res =await fetch("https://jsonplaceholder.typicode.com/posts")
+   var dats =await res.json();
+  console.log(data);
+}
+catch (err) {
+    console.log(err);
+    }
+}
+getData();
