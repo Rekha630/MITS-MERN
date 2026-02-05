2b40a286b06911607a2f5182924234a9b5a16a13
rekha <rekharoyal282@gmail.com>
2026-02-05 12:33:04 +0530

    Backend Api Created

---
+++ backend/Server.js
@@ -0,0 +1,67 @@
+const express = require("express")
+const todoRoute = require("./router/todoRouter")
+const dotenv = require("dotenv")
+const userRoute = require("./router/userRouter");
+const connectDB = require("./config/db")
+const cors = require("cors")
+
+dotenv.config();
+const app = express();
+app.use(cors());
+connectDB();
+
+app.use(express.json())
+app.use("/api/todo", todoRoute);
+app.use("/api/users",userRoute);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,()=>{
+    console.log(`Server running on http://localhost:${PORT}`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//app.get("/",(req,res)=>{
+   // res.send("Get Route is Working")
+//})
+//app.post("/create",(req,res)=>{
+   // res.json({
+       // message:"Post Route is Working"
+    //})
+//})
+//app.put("/",(req,res)=>{
+   // res.status(200).json({
+   // message:"Put Route is Working"
+   // })
+//})
+//app.delete("/",(req,res)=>{
+    //res.status(200).json({
+        //message:"Delete Route is Working"
+    //})
+//})
+//app.options("/",(req,res)=>{
+    //res.status(200).json({
+        //message:"Options Route is Working"
+    //})
+//})
+//app.head("/",(req,res)=>{
+    //res.status(200).json({
+       // message:"Head Route is Working"
+    //})
+//})
+//app.patch("/",(req,res)=>{
+    //res.status(200).json({
+        //message:"Patch Route is Working"
+    //})
+//})
