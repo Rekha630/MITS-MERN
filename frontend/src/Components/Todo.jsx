8fd48836d3cc6794f4a0b91856851a832c1d4d81
rekha <rekharoyal282@gmail.com>
2026-02-06 10:42:04 +0530

    backend and frontend

--- frontend/src/Components/Todo.jsx
+++ frontend/src/Components/Todo.jsx
@@ -6,44 +6,41 @@ const Todo = () => {
     const[task,setTask]=useState("");
     const[todos,setTodos]=useState([]);
     const[editing,setEditing]=useState(null);
+    const API = "http://localhost:3000";
 
     const fetchData = async(req,res)=>{
-        const responce = await axios.get(`http://localhost:3000/api/todo/`)
+        const responce = await axios.get(`${API}/api/todo/`)
         setTodos(responce.data);
     }
     useEffect(()=>{
         fetchData();
     },[])
-    const handleAddOrUpdate=(e)=>{
+
+    const handleAddOrUpdate=async(e)=>{
         e.preventDefault();
         if(editing!==null){
-            const updatedTodo = todos;
-             updatedTodo[editing].task=task;
-             setTodos(updatedTodo);
-             setEditing(null)
-
-        }else{
-            const newTodo={
-                task:task,
-                completed:false
+            await axios.put(`${API}/api/todo/update/${editing}`,{task})
+            
+             setEditing(null);
+            }else{
+                 await axios.post(`${API}/api/todo/create`,{task})
             }
-            setTodos([...todos,newTodo]); 
-        }
         setTask("");
+        fetchData();
     };
-    const handleDelete=(index)=>{
-        const updatedTodo=todos.filter((__,i)=>i!==index);
-        setTodos(updatedTodo);
+    const handleDelete=async(id)=>{
+        await axios.delete(`${API}/api/todo/delete/${id}`)
+        fetchData();
     };
-    const handleToggleComplete=(index)=>{
-        const updatedTodo=todos;
-        updatedTodo[index].completed=!updatedTodo[index].completed;
-        setTodos(updatedTodo)
-
+    const handleToggleComplete=async(todo)=>{
+        await axios.put(`${API}/api/todo/update/${todo._id}`,{
+        completed: !todo.completed
+    })
+    fetchData();
     };
-    const handleEdit=(index)=>{
-        setTask(todos[index].task);
-        setEditing(index);
+    const handleEdit=(todo)=>{
+        setTask(todo.task);
+        setEditing(todo._id);
     }
   return (
     <div>
@@ -55,11 +52,11 @@ const Todo = () => {
       </form>
       <ul>
         {todos.map((todo,index)=>(
-
-            <li key={index}>
-                <span style={{cursor:"pointer",textDecoration:todo.completed?"line-through":"none"}}onClick={()=>handleToggleComplete(index)}>{todo.task}</span>
-                <button onClick={()=>handleEdit(index)}>Edit</button>
-                <button onClick={()=>handleDelete(index)}>Delete</button>
+            <li key={todo._id}>
+                <span style={{cursor:"pointer",textDecoration:todo.completed?"line-through":"none"}}
+                onClick={()=>handleToggleComplete(todo)}>{todo.task}</span>
+                <button onClick={()=>handleEdit(todo)}>Edit</button>
+                <button onClick={()=>handleDelete(todo._id)}>Delete</button>
             </li>
         
         ))}
